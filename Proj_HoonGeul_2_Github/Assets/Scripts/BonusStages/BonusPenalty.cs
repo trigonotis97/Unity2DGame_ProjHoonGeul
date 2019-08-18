bda8e96c19aaad7ba5e74cfb4793edbd59242985
yul10001 <skys1191@gmail.com>
2019-08-18 20:48:45 +0900

    깜지 씬 제작
    했어요

--- Proj_HoonGeul_2_Github/Assets/Scripts/BonusStages/BonusPenalty.cs
+++ Proj_HoonGeul_2_Github/Assets/Scripts/BonusStages/BonusPenalty.cs
@@ -8,47 +8,65 @@ public class BonusPenalty : MonoBehaviour
 {
     GameManager m_gameManager;
 
-    public Text problem;
+    //public Text problem;
     public InputField InputText;
+    public Text[] texts;
+    int nowLine;
 
     public SceneData sceneData;
+    
 
-    int selectAns;
-    string[,] answerStr = new string[2, 8] {{"동해물과 백두산이 마르고 닳도록",
+    //int selectAns;
+    string[] answerStr = new string[6] {"동해물과 백두산이 마르고 닳도록",
                                         "하느님이 보우하사 우리나라 만세",
-                                        "무궁화 삼천리 화려강산",
-                                        "대한 사람 대한으로 길이 보전하세",
                                         "남산 위에 저 소나무 철갑을 두른 듯",
                                         "바람서리 불변함은 우리 기상일세",
                                         "무궁화 삼천리 화려강산",
-                                        "대한 사람 대한으로 길이 보전하세"},
-                                          {"가을 하늘 공활한데 높고 구름 없이",
-                                        "밝은 달은 우리 가슴 일편단심일세",
-                                        "무궁화 삼천리 화려강산",
-                                        "대한 사람 대한으로 길이 보전하세",
-                                        "이 기상과 이 맘으로 충성을 다하여",
-                                        "괴로우나 즐거우나 나라 사랑하세",
-                                        "무궁화 삼천리 화려강산",
-                                        "대한 사람 대한으로 길이 보전하세"}};
-    string[] nowAnsStr = new string[8];
-
+                                        "대한 사람 대한으로 길이 보전하세"};
     void Start()
     {
         m_gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         sceneData = m_gameManager.GetSceneData();
-        AnsGenerator();
+        nowLine = 0;
+        //AnsGenerator();
     }
-
-    public void AnsGenerator()
+    public void Check()
     {
-        selectAns = Random.Range(0, 2);
-        for (int i = 0; i < 8; i++)
+        if (InputText.text == answerStr[nowLine])
         {
-            nowAnsStr[i] = answerStr[selectAns, i]; //중간 매개 배열
-        }
+            nowLine += 1;
+            if (nowLine == answerStr.Length)
+            {
+                Debug.Log("다음 씬으로 보내주자");
+            }
+            else { 
+            
+            InputText.textComponent = texts[nowLine];
+            InputText.text = "";
+            Debug.Log("일치!");
+            }
 
-        //problem.GetComponent<Text>().text = nowAns;
+        }
+        else
+        {
+            InputText.text = "";
+            Debug.Log("틀렸어요");
+        }
     }
+    public void AdButton()
+    {
+        //광고 재생 후 다음 씬으로
+    }
+    //public void AnsGenerator()
+    //{
+    //    selectAns = Random.Range(0, 2);
+    //    for (int i = 0; i < 8; i++)
+    //    {
+    //        nowAnsStr[i] = answerStr[selectAns, i]; //중간 매개 배열
+    //    }
+
+    //    //problem.GetComponent<Text>().text = nowAns;
+    //}
 
     /*public void textInputEnter()
     {
