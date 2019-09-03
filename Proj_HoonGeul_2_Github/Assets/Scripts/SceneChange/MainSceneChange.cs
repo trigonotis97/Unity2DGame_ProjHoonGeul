3062fcfa9f31b2a347b930d243b7ed178e23f67b
yul10001 <skys1191@gmail.com>
2019-09-03 23:16:43 +0900

    메인화면 꾸미는중
    버튼 연결, 씬이동 애니메이션 제작중

---
+++ Proj_HoonGeul_2_Github/Assets/Scripts/SceneChange/MainSceneChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneChange : MonoBehaviour
+{
+    static public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nextScene = "";
+    }
+
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    //public void StoryLoad()
+    //{
+    //    SceneManager.LoadScene("DialogScene");
+    //}
+    //public void LastStroyLoad()
+    //{
+    //    SceneManager.LoadScene("LastStoryMode");
+    //}
+    //public void GgamJiLoad()
+    //{
+    //    SceneManager.LoadScene("GgamJiMode");
+    //}
+    //public void PuzzleLoad()
+    //{
+    //    SceneManager.LoadScene("PuzzleMode");
+    //}
+    //public void RankLoad()
+    //{
+    //    SceneManager.LoadScene("RankMode");
+    //}
+}
