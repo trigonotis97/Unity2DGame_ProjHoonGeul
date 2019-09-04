1bcc898362b1740caf77eb7efb821e3d86d6d1c2
yul10001 <skys1191@gmail.com>
2019-09-04 21:05:25 +0900

    깜지 씬 개편
    1절부터 4절까지 추가하고 배치 변경.
    아직은 랜덤인데, 게임매니저 통해서 순서대로 돌아가며 나오는 기능 필요!

--- Proj_HoonGeul_2_Github/Assets/Scripts/BonusStages/BonusPenalty.cs
+++ Proj_HoonGeul_2_Github/Assets/Scripts/BonusStages/BonusPenalty.cs
@@ -7,40 +7,61 @@ using UnityEngine.SceneManagement;
 public class BonusPenalty : MonoBehaviour
 {
     GameManager m_gameManager;
-    public ForTest_GoBack ForTest_GoBack;
     //public Text problem;
     public InputField InputText;
     public Text[] texts;
     int nowLine;
+    public Text answerSpace;
 
     public SceneData sceneData;
     public AudioSource AudioSource;
     public AudioClip correct, incorrect;
-    
 
-    //int selectAns;
-    string[] answerStr = new string[6] {"동해물과 백두산이 마르고 닳도록",
-                                        "하느님이 보우하사 우리나라 만세",
-                                        "남산 위에 저 소나무 철갑을 두른 듯",
-                                        "바람서리 불변함은 우리 기상일세",
-                                        "무궁화 삼천리 화려강산",
-                                        "대한 사람 대한으로 길이 보전하세"};
+    public int questionNum;
+    public string[,] answerStr = new string[4, 4]
+    {
+        {"동해물과 백두산이 마르고 닳도록",
+        "하느님이 보우하사 우리나라 만세",
+        "무궁화 삼천리 화려강산",
+        "대한 사람 대한으로 길이 보전하세"},
+        {"남산 위에 저 소나무 철갑을 두른 듯",
+        "바람서리 불변함은 우리 기상일세",
+        "무궁화 삼천리 화려강산",
+        "대한 사람 대한으로 길이 보전하세"},
+        {"가을 하늘 공활한데 높고 구름 없이",
+        "밝은 달은 우리 가슴 일편단심일세",
+        "무궁화 삼천리 화려강산",
+        "대한 사람 대한으로 길이 보전하세"},
+        {"이 기상과 이 맘으로 충성을 다하여",
+        "괴로우나 즐거우나 나라 사랑하세",
+        "무궁화 삼천리 화려강산",
+        "대한 사람 대한으로 길이 보전하세"}
+    };
+
     void Start()
     {
         m_gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         sceneData = m_gameManager.GetSceneData();
         nowLine = 0;
-        //AnsGenerator();
+        
+        questionNum = Random.Range(0, 4);
+        //일단 랜덤으로 뽑았는데, 게임데이터 이용해서 순서대로 돌려야해요.
+        answerSpace.text = "";
+        for (int i = 0; i < 4; i++)
+        {
+            answerSpace.text += answerStr[questionNum, i] + "\n";
+        }
+       
     }
     public void Check()
     {
-        if (InputText.text == answerStr[nowLine])
+        if (InputText.text == answerStr[questionNum,nowLine])
         {
             //정답 사운드
             AudioSource.clip = correct;
             AudioSource.PlayOneShot(correct);
             nowLine += 1;
-            if (nowLine == answerStr.Length)
+            if (nowLine == 4)
             {
                 Debug.Log("다음 씬으로 보내주자");
                 m_gameManager.SetCurrentSceneKey(m_gameManager.GetCurrentSceneKey() - 2);
